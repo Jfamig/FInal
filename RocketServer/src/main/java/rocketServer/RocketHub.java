54d753b6c804232e07e74e1a8b8c9b25e63fb2d3
BuildTools <unconfigured@null.spigotmc.org>
2016-12-15 15:10:24 -0500

    Final Code

--- RocketServer/src/main/java/rocketServer/RocketHub.java
+++ RocketServer/src/main/java/rocketServer/RocketHub.java
@@ -11,7 +11,6 @@ import rocketData.LoanRequest;
 public class RocketHub extends Hub {
 
 	private RateBLL _RateBLL = new RateBLL();
-	
 	public RocketHub(int port) throws IOException {
 		super(port);
 	}
@@ -22,6 +21,7 @@ public class RocketHub extends Hub {
 		
 		if (message instanceof LoanRequest) {
 			resetOutput();
+			
 			
 			LoanRequest lq = (LoanRequest) message;
 			
@@ -37,10 +37,15 @@ public class RocketHub extends Hub {
 			try {
 				int creditScore = lq.getiCreditScore();
 				lq.setdRate(RateBLL.getRate(creditScore));
+				
 			} catch (RateException e) {
 				sendToAll(e);
+				
 				System.out.println("Error: Credit Score not applicable");
 			}
+			
+			
+			
 			double payment = RateBLL.getPayment(lq.getdRate()/100,lq.getiTerm()*12,lq.getdAmount()-lq.getiDownPayment(), 0.0, false);
 			lq.setdPayment(payment);
 			
